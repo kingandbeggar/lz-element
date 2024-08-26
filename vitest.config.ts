69a3a06b263665ca2b0dd51e53cee36824f70ec7
lzw <lzw@gmail.com>
2024-08-26 23:57:51 +0800

    commit all

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+/// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx' 
-// https://vitejs.dev/config/
+import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
